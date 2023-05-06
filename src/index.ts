27d73104da6e49d6be77d7b6683ae11b769c313a
sd <leowest1921@gmail.com>
2023-05-06 19:53:23 +0800

    add

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import type { PluginOption } from './types';
  * A vite plugin for vue, auto generate route info by page
  * @param options plugin options
  */
-function pageRoute(options?: Partial<PluginOption>) {
+export function pageRoute(options?: Partial<PluginOption>) {
   const context = new Context(options);
 
   const plugin: Plugin = {
